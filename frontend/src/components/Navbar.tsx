ef5028af6272c535f2c078db2f1cdeedec74ff2f
aungzm <azm.haysharm@gmail.com>
2025-01-11 02:38:27 -0500

    Feat: Navbar position is now fixed. Logo on left most and on Account and Profile  right most

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -50,30 +50,30 @@ const Navbar = () => {
     };
 
     return (
-        <nav className="bg-black fixed w-full z-20 top-0 start-0 border-b">
-            {/* Container */}
-            <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-                {/* LOGO */}
+        <nav className="bg-black fixed w-full top-0 start-0 border-b">
+            {/* Outer container */}
+            <div className="flex items-center justify-between mx-auto p-4">
+                
+                {/* LOGO on the left */}
                 <a
-                    href="/"
-                    className="flex items-center space-x-3 rtl:space-x-reverse"
+                href="/"
+                className="flex items-center space-x-3 rtl:space-x-reverse"
                 >
-                    <img
-                        src="https://flowbite.com/docs/images/logo.svg"
-                        className="h-8"
-                        alt="Flowbite Logo"
-                    />
-                    <span className="self-center text-white text-2xl font-semibold whitespace-nowrap dark:text-white">
-                        DropIn
-                    </span>
+                <img
+                    src="https://flowbite.com/docs/images/logo.svg"
+                    className="h-8"
+                    alt="Flowbite Logo"
+                />
+                <span className="self-center text-white text-2xl font-semibold whitespace-nowrap dark:text-white">
+                    DropIn
+                </span>
                 </a>
-
-                {/* Account Name and Logo */}
-                <div className="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse relative">
+                {/* Account Name and Profile on the right */}
+                <div className="flex items-center space-x-3 relative">
                     <button
                         type="button"
                         onClick={toggleDropdown}
-                        className="inline-flex items-center font-medium justify-center px-4 py-2 text-sm text-white rounded-lg cursor-pointer hover:bg-gray-100 hover:text-black"
+                        className="inline-flex items-center font-medium px-4 py-2 text-sm text-white rounded-lg cursor-pointer hover:bg-gray-100 hover:text-black"
                     >
                         {/* Profile Picture */}
                         {profilePic ? (
